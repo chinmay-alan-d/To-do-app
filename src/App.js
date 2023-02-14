7eef9689a72b3c5ee06bd035f563be763331754a
Chinmay Aland <chinmay@Chinmays-MacBook-Air.local>
2023-02-14 11:08:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
 
 function App() {
+  const [notes, setnotes] = useState(['hi first', 'hi second']);
+  const [note, setnote] = useState("");
+
+  const handleMe = (e) => {
+    if (note !== ""){
+      setnotes(notes => [...notes, note]);
+      setnote('');
+    }else{
+      alert("Write a note then create it!!")
+    }
+  }
+  const handleRemoveItem = name => {
+    setnotes( old => {
+      return old.filter(y => y !== name)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <br />
+      <div className="card" style={{maxWidth : '40rem', left : '25rem'}}>
+        <h5 style={ {textAlign : 'center'} }>Create A note</h5>
+        <input class="form-control"  aria-describedby="emailHelp" type="text" style={{maxWidth : '50rem', maxHeight : '20rem'}} onChange={(e) => { setnote(e.target.value) }}></input><br />
+        <button placeholder="Create Note" onClick={handleMe} className="btn btn-primary" rows="3">Create Note</button>
+      </div>
+      <br />
+      <div className="container" style={{left : '500rem'}}>
+        <div className="row 3">
+          {
+            notes.map((eachnote, i) => {
+              return (
+                <div className="col-4 card" style={{ maxWidth: "20rem", minWidth: "20rem" }} key={i}>
+                  <div className="card-body">
+                    <p className="card-text">{eachnote}</p>
+                    <button onClick={() =>handleRemoveItem(eachnote)}>delete</button>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    </div >
   );
 }
-
 export default App;
